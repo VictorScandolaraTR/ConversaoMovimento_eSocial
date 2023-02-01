484da19fbd12b2e9ad9f17066eeb8e1eb877f493
VictorScandolaraTR <84342800+VictorScandolaraTR@users.noreply.github.com>
2023-02-01 17:50:33 -0300

    Aplicação do SQLite

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QDialog
 from PyQt5 import QtCore, QtGui, QtWidgets
 from src.esocial import eSocialXML
 from src.ui.esocial_unico import Ui_MainWindow as Interface
+from src.ui.dialog_configuaracoes import Ui_dialog_configuracioes as DialogConfiguracoes
+from sqlalchemy import create_engine, Table, update
+import pandas as pd
 import os, json, sys
 
 class eSocial(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.__window = Interface()
-        self.__esocial = eSocialXML("xml")
+        self.__diretorio_trabalho = "xml"
+        self.__engine = create_engine(f"sqlite:///{self.__diretorio_trabalho}/operacao.db")
         self.__empresas, self.__empresas_em_uso = self.carrega_empresas()
     
     def init_gui(self):
@@ -21,49 +25,64 @@ class eSocial(QtWidgets.QMainWindow):
         self.__tabela_empresas = self.__window.tableWidget
         self.__edit_inscricao = self.__window.edit_inscricao
         self.__btn_adiciona_empresa = self.__window.btn_adiciona_empresa
+        self.__btn_conexoes = self.__window.btn_conexoes
         self.__btn_obtem_dados_esocial = self.__window.btn_obtem_dados_esocial
         self.__btn_relaciona_empresas_dominio = self.__window.btn_relaciona_empresas_dominio
         self.__btn_relacionar_rubricas = self.__window.btn_relacionar_rubricas
         self.__btn_executa_rpa = self.__window.btn_executa_rpa
+        self.__btn_excluir = self.__window.btn_excluir
 
         self.__tabela_empresas.setColumnWidth(2,344)
         self.__tabela_empresas.setColumnWidth(3,200)
 
         self.__tabela_empresas.clicked.connect(self.seleciona_registro)
         self.__btn_adiciona_empresa.clicked.connect(self.adiciona_empresa)
+        self.__btn_conexoes.clicked.connect(self.configura_conexoes)
         self.__btn_obtem_dados_esocial.clicked.connect(self.acessa_portal_esocial)
         self.__btn_relaciona_empresas_dominio.clicked.connect(self.relaciona_empresa_dominio)
         self.__btn_relacionar_rubricas.clicked.connect(self.relacionar_rubricas)
         self.__btn_executa_rpa.clicked.connect(self.executar_rpa)
+        self.__btn_excluir.clicked.connect(self.exclui_empresa)
 
+        self.__btn_conexoes.setEnabled(False)
         self.__btn_obtem_dados_esocial.setEnabled(False)
         self.__btn_relaciona_empresas_dominio.setEnabled(False)
         self.__btn_relacionar_rubricas.setEnabled(False)
         self.__btn_executa_rpa.setEnabled(False)
+        self.__btn_excluir.setEnabled(False)
 
         self.atualiza_tabela_empresas()
         self.show()
 
     def carrega_empresas(self):
-        try:
-            f = open(f"{self.__esocial.DIRETORIO_RAIZ}\\empresas.json","r")
-            empresas_texto = f.readline()
-            f.close
-            empresas = json.loads(empresas_texto)
+        df = pd.read_sql("SELECT * FROM EMPRESAS",con=self.__engine)
+        
+        empresas = {}
+
+        for i in range(len(df)):
+            empresas[str(i)] = {
+                "inscricao": df.loc[i,"inscricao"],
+                "codi_emp": df.loc[i,"codi_emp"],
+                "nome_emp": df.loc[i,"nome_emp"],
+                "status": df.loc[i,"status"],
+                "base_dominio": df.loc[i,"base_dominio"],
+                "usuario_dominio": df.loc[i,"usuario_dominio"],
+                "senha_dominio": df.loc[i,"senha_dominio"],
+                "empresa_padrao_rubricas": df.loc[i,"empresa_padrao_rubricas"],
+                "usuario_esocial": df.loc[i,"usuario_esocial"],
+                "senha_esocial": df.loc[i,"senha_esocial"],
+                "certificado_esocial": df.loc[i,"certificado_esocial"],
+                "tipo_certificado_esocial": df.loc[i,"tipo_certificado_esocial"],
+                "usuario_sgd": df.loc[i,"usuario_sgd"],
+                "senha_sgd": df.loc[i,"senha_sgd"]
+            }
             
-            empresas_em_uso = []
+        empresas_em_uso = []
 
-            for i in empresas:
-                empresas_em_uso.append(empresas[i]["inscricao"])
+        for i in empresas:
+            empresas_em_uso.append(empresas[i]["inscricao"])
 
-            return empresas, empresas_em_uso
-        except:
-            return {}, []
-
-    def salva_empresas(self):
-        f = open(f"{self.__esocial.DIRETORIO_RAIZ}\\empresas.json","w")
-        f.write(json.dumps(self.__empresas))
-        f.close
+        return empresas, empresas_em_uso
 
     def atualiza_tabela_empresas(self):
         quantidade_empresas = len(self.__empresas)
@@ -82,13 +101,13 @@ class eSocial(QtWidgets.QMainWindow):
                 status = self.__empresas.get(str(linha)).get("status")
 
                 match status:
-                    case "e-Social":
+                    case "E":
                         status = "Dados carregados do e-Social"
-                    case "Dominio":
+                    case "D":
                         status = "Dados carregados do Domínio"
-                    case "Rubricas":
+                    case "P":
                         status = "Planilha de rubricas geradas"
-                    case "RPA":
+                    case "R":
                         status = "Processo concluído"
 
                 self.__tabela_empresas.setItem(linha, 3, QtWidgets.QTableWidgetItem(status))
@@ -96,6 +115,7 @@ class eSocial(QtWidgets.QMainWindow):
                 linha = linha + 1
     
     def adiciona_empresa(self):
+        os.system(f"mkdir {self.__diretorio_trabalho}")
         proxima_linha = len(self.__empresas)
         inscricao = self.__edit_inscricao.text()
 
@@ -105,89 +125,142 @@ class eSocial(QtWidgets.QMainWindow):
         if(inscricao in self.__empresas_em_uso):
             print("Empresa já cadastrada.")
             return
+        
+        os.system(f"mkdir {self.__diretorio_trabalho}\\{inscricao}")
+        os.system(f"mkdir {self.__diretorio_trabalho}\\{inscricao}\\downloads")
+        os.system(f"mkdir {self.__diretorio_trabalho}\\{inscricao}\\eventos")
+        os.system(f"mkdir {self.__diretorio_trabalho}\\{inscricao}\\importar")
+
+        conexao = self.__engine.connect()
+        conexao.execute(f"INSERT INTO EMPRESAS (inscricao) VALUES ('{inscricao}')")
+        self.atualiza_tabela_empresas()
 
         self.__empresas[str(proxima_linha)] = {
             "inscricao": inscricao,
             "codi_emp": "",
             "nome_emp": "",
             "status": "",
-            "configuracoes":{
-                "base_dominio": "",
-                "usuario_dominio": "",
-                "senha_dominio": "",
-                "empresa_padrao_rubricas": "",
-                "usuario_esocial": "",
-                "senha_esocial": "",
-                "certificado_esocial": "",
-                "tipo_certificado_esocial": "",
-                "usuario_sgd": "",
-                "senha_sgd": ""
-            }
+            "base_dominio": "",
+            "usuario_dominio": "",
+            "senha_dominio": "",
+            "empresa_padrao_rubricas": "",
+            "usuario_esocial": "",
+            "senha_esocial": "",
+            "certificado_esocial": "",
+            "tipo_certificado_esocial": "",
+            "usuario_sgd": "",
+            "senha_sgd": ""
         }
 
         self.__empresas_em_uso.append(inscricao)
         
         self.__edit_inscricao.setText("")
-        self.salva_empresas()
         self.atualiza_tabela_empresas()
 
+    def exclui_empresa(self):
+        self.atualiza_status("Excluindo...")
+        inscricao = self.__tabela_empresas.selectedItems()[0].text()
+
+        conexao = self.__engine.connect()
+        conexao.execute(f"DELETE FROM EMPRESAS WHERE inscricao = {inscricao}")
+
+        os.system(f"del /q {self.__diretorio_trabalho}\\{inscricao}\\downloads")
+        os.system(f"del /q {self.__diretorio_trabalho}\\{inscricao}\\eventos")
+        os.system(f"del /q {self.__diretorio_trabalho}\\{inscricao}\\importar")
+        os.system(f"del /q {self.__diretorio_trabalho}\\{inscricao}")
+        os.system(f"rmdir {self.__diretorio_trabalho}\\{inscricao}\\downloads")
+        os.system(f"rmdir {self.__diretorio_trabalho}\\{inscricao}\\eventos")
+        os.system(f"rmdir {self.__diretorio_trabalho}\\{inscricao}\\importar")
+        os.system(f"rmdir {self.__diretorio_trabalho}\\{inscricao}")
+        
+        self.__empresas, self.__empresas_em_uso = self.carrega_empresas()
+        self.atualiza_tabela_empresas()
+
+    def configura_conexoes(self):
+        window = eSocialConfiguracoes()
+        window.init_gui()
+    
     def seleciona_registro(self):
         indice = self.__tabela_empresas.selectedItems()[0].row()
         status = self.__empresas[str(indice)]["status"]
 
         match status:
-            case "":
+            case ""|None:
+                self.__btn_conexoes.setEnabled(True)
                 self.__btn_obtem_dados_esocial.setEnabled(True)
                 self.__btn_relaciona_empresas_dominio.setEnabled(False)
                 self.__btn_relacionar_rubricas.setEnabled(False)
                 self.__btn_executa_rpa.setEnabled(False)
-            case "e-Social":
+                self.__btn_excluir.setEnabled(True)
+            case "E":
+                self.__btn_conexoes.setEnabled(True)
                 self.__btn_obtem_dados_esocial.setEnabled(True)
                 self.__btn_relaciona_empresas_dominio.setEnabled(True)
                 self.__btn_relacionar_rubricas.setEnabled(False)
                 self.__btn_executa_rpa.setEnabled(False)
+                self.__btn_excluir.setEnabled(True)
                 self.__tabela_empresas.selectedItems()[3].setText("Dados carregados do e-Social")
-            case "Dominio":
+            case "D":
+                self.__btn_conexoes.setEnabled(True)
                 self.__btn_obtem_dados_esocial.setEnabled(True)
                 self.__btn_relaciona_empresas_dominio.setEnabled(True)
                 self.__btn_relacionar_rubricas.setEnabled(True)
                 self.__btn_executa_rpa.setEnabled(False)
+                self.__btn_excluir.setEnabled(True)
                 self.__tabela_empresas.selectedItems()[3].setText("Dados carregados do Domínio")
-            case "Rubricas":
+            case "P":
+                self.__btn_conexoes.setEnabled(True)
                 self.__btn_obtem_dados_esocial.setEnabled(True)
                 self.__btn_relaciona_empresas_dominio.setEnabled(True)
                 self.__btn_relacionar_rubricas.setEnabled(True)
                 self.__btn_executa_rpa.setEnabled(False)
+                self.__btn_excluir.setEnabled(True)
                 self.__tabela_empresas.selectedItems()[3].setText("Planilha de rubricas geradas")
-            case "RPA":
+            case "R":
+                self.__btn_conexoes.setEnabled(True)
                 self.__btn_obtem_dados_esocial.setEnabled(True)
                 self.__btn_relaciona_empresas_dominio.setEnabled(True)
                 self.__btn_relacionar_rubricas.setEnabled(True)
                 self.__btn_executa_rpa.setEnabled(True)
+                self.__btn_excluir.setEnabled(True)
                 self.__tabela_empresas.selectedItems()[3].setText("Processo concluído")
 
-    def atualiza_status(self, status):
+    def atualiza_status(self, status, status_bd = ""):
         self.__tabela_empresas.selectedItems()[3].setText(status)
+        inscricao = self.__tabela_empresas.selectedItems()[0].text()
+        indice = self.__tabela_empresas.selectedItems()[0].row()
+
+        if(status_bd!=""):
+            self.__empresas[str(indice)]["status"] = "E"
+            conexao = self.__engine.connect()
+            conexao.execute(f"UPDATE EMPRESAS SET status = '{status_bd}' WHERE inscricao = '{inscricao}'")
     
     def acessa_portal_esocial(self):
         self.atualiza_status("Buscando dados do e-Social...")
-        self.__esocial.configura_conexao_esocial("usuario","senha","certificado")
-        self.__esocial.baixar_dados_esocial()
+        indice = self.__tabela_empresas.selectedItems()[0].row()
+        inscricao = self.__tabela_empresas.selectedItems()[0].text()
+        esocial = eSocialXML(f"{self.__diretorio_trabalho}\\{inscricao}")
+
+        usuario = self.__empresas.get(indice).get("usuario_esocial")
+        senha = self.__empresas.get(indice).get("senha_esocial")
+        tipo = self.__empresas.get(indice).get("tipo_certificado_esocial")
+        certificado = self.__empresas.get(indice).get("certificado_esocial")
+        esocial.configura_conexao_esocial(usuario, senha, certificado, tipo)
+        esocial.baixar_dados_esocial()
         
         self.atualiza_status("Lendo informações...")
-        self.__esocial.extrair_arquivos_xml()
-        self.__esocial.carregar_informacoes_xml()
+        esocial.extrair_arquivos_xml()
+        esocial.carregar_informacoes_xml()
 
-        self.atualiza_status("Dados carregados do e-Social")
-        indice = self.__tabela_empresas.selectedItems()[0].row()
-        self.__empresas[str(indice)]["status"] = "e-Social"
-        self.salva_empresas()
+        self.atualiza_status("Dados carregados do e-Social","E")
         self.seleciona_registro()
 
     def relaciona_empresa_dominio(self):
         self.atualiza_status("Consultando banco de dados Domínio...")
-        self.__esocial.configura_conexao_dominio("movto_esocial","EXTERNO","123456")
-        empresas = self.__esocial.relaciona_empresas(str(self.__tabela_empresas.selectedItems()[0].text()))
+        inscricao = self.__tabela_empresas.selectedItems()[0].text()
+        esocial = eSocialXML(f"{self.__diretorio_trabalho}\\{inscricao}")
+        esocial.configura_conexao_dominio("movto_esocial","EXTERNO","123456")
+        empresas = esocial.relaciona_empresas(str(self.__tabela_empresas.selectedItems()[0].text()))
         indice = self.__tabela_empresas.selectedItems()[0].row()
 
         if(len(empresas)==1):
@@ -202,60 +275,37 @@ class eSocial(QtWidgets.QMainWindow):
             self.__empresas[str(indice)]["codi_emp"] = str(empresas[0]["codigo"])
             self.__empresas[str(indice)]["nome_emp"] = str(empresas[0]["nome"])
 
-        self.atualiza_status("Dados carregados do Domínio")
-        self.__empresas[str(indice)]["status"] = "Dominio"
-        self.salva_empresas()
+        self.atualiza_status("Dados carregados do Domínio","D")
+        self.seleciona_registro()
 
     def relacionar_rubricas(self):
         indice = self.__tabela_empresas.selectedItems()[0].row()
         self.atualiza_status("Gerando planilha de relações de rubricas...")
-        self.__esocial.carregar_informacoes_xml()
-        self.__esocial.gera_excel_relacao(self.__tabela_empresas.selectedItems()[0].text())
-        self.atualiza_status("Planilha de rubricas geradas")
-        self.__empresas[str(indice)]["status"] = "Rubricas"
-        self.salva_empresas()
+        inscricao = self.__tabela_empresas.selectedItems()[0].text()
+        esocial = eSocialXML(f"{self.__diretorio_trabalho}\\{inscricao}")
+        esocial.carregar_informacoes_xml()
+        esocial.gera_excel_relacao(self.__tabela_empresas.selectedItems()[0].text())
+        self.atualiza_status("Planilha de rubricas geradas","P")
+        self.seleciona_registro()
 
     def executar_rpa(self):
         self.atualiza_status("Função RPA não configurada")
+
+class eSocialConfiguracoes(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.__window = DialogConfiguracoes()
+    
+    def init_gui(self):
+        """
+        Declarar componentes que serão utilizados da interface
+        """
+        self.__window.setupUi(self)
+        self.setWindowTitle('Configuração de credenciais')
+        self.show()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     window = eSocial()
     window.init_gui()
     app.exec()
-
-'''os.system("cls")
-esocial = eSocialXML("xml")
-esocial.carregar_informacoes_xml()
-
-relacao_empresas = esocial.relaciona_empresas()
-relacao_empregados = esocial.relaciona_empregados()
-
-esocial.gera_excel_relacao(relacao_empresas)
-
-esocial.gerar_afastamentos_importacao()
-esocial.gerar_ferias_importacao()
-
-f = open("s1010.json","w")
-f.write(json.dumps(esocial.processar_rubricas()))
-f.close()
-
-f = open("s1200.json","w")
-f.write(json.dumps(esocial.dicionario_s1200))
-f.close()
-
-f = open("s1210.json","w")
-f.write(json.dumps(esocial.dicionario_s1210))
-f.close()
-
-f = open("s2230.json","w")
-f.write(json.dumps(esocial.dicionario_s2230))
-f.close()
-
-f = open("s2299.json","w")
-f.write(json.dumps(esocial.dicionario_s2299))
-f.close()
-
-f = open("s2399.json","w")
-f.write(json.dumps(esocial.dicionario_s2399))
-f.close()'''
--- src/ui/esocial_unico.py
+++ src/ui/esocial_unico.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'esocial_unico.ui'
+# Form implementation generated from reading ui file 'screen_ui\esocial_unico.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.8
 #
@@ -14,21 +14,21 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(941, 524)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 60, 47, 13))
+        self.label.setGeometry(QtCore.QRect(20, 20, 201, 16))
         self.label.setObjectName("label")
         self.btn_adiciona_empresa = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_adiciona_empresa.setGeometry(QtCore.QRect(180, 60, 75, 21))
+        self.btn_adiciona_empresa.setGeometry(QtCore.QRect(330, 20, 75, 21))
         self.btn_adiciona_empresa.setObjectName("btn_adiciona_empresa")
         self.edit_inscricao = QtWidgets.QLineEdit(self.centralwidget)
-        self.edit_inscricao.setGeometry(QtCore.QRect(70, 59, 113, 20))
+        self.edit_inscricao.setGeometry(QtCore.QRect(220, 19, 113, 20))
         self.edit_inscricao.setObjectName("edit_inscricao")
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
         self.tableWidget.setEnabled(True)
-        self.tableWidget.setGeometry(QtCore.QRect(15, 101, 761, 451))
+        self.tableWidget.setGeometry(QtCore.QRect(15, 61, 761, 451))
         self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.tableWidget.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
         self.tableWidget.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
@@ -44,17 +44,23 @@ class Ui_MainWindow(object):
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(3, item)
         self.btn_obtem_dados_esocial = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_obtem_dados_esocial.setGeometry(QtCore.QRect(20, 560, 150, 21))
+        self.btn_obtem_dados_esocial.setGeometry(QtCore.QRect(780, 90, 150, 21))
         self.btn_obtem_dados_esocial.setObjectName("btn_obtem_dados_esocial")
         self.btn_executa_rpa = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_executa_rpa.setGeometry(QtCore.QRect(620, 560, 150, 21))
+        self.btn_executa_rpa.setGeometry(QtCore.QRect(780, 180, 150, 21))
         self.btn_executa_rpa.setObjectName("btn_executa_rpa")
         self.btn_relacionar_rubricas = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_relacionar_rubricas.setGeometry(QtCore.QRect(420, 560, 150, 21))
+        self.btn_relacionar_rubricas.setGeometry(QtCore.QRect(780, 150, 150, 21))
         self.btn_relacionar_rubricas.setObjectName("btn_relacionar_rubricas")
         self.btn_relaciona_empresas_dominio = QtWidgets.QPushButton(self.centralwidget)
-        self.btn_relaciona_empresas_dominio.setGeometry(QtCore.QRect(220, 560, 150, 21))
+        self.btn_relaciona_empresas_dominio.setGeometry(QtCore.QRect(780, 120, 150, 21))
         self.btn_relaciona_empresas_dominio.setObjectName("btn_relaciona_empresas_dominio")
+        self.btn_excluir = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_excluir.setGeometry(QtCore.QRect(780, 210, 150, 21))
+        self.btn_excluir.setObjectName("btn_excluir")
+        self.btn_conexoes = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_conexoes.setGeometry(QtCore.QRect(780, 60, 150, 21))
+        self.btn_conexoes.setObjectName("btn_conexoes")
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -63,7 +69,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Inscrição"))
+        self.label.setText(_translate("MainWindow", "Inscrição (apenas raiz, no caso de CNPJ)"))
         self.btn_adiciona_empresa.setText(_translate("MainWindow", "Adicionar"))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "Inscrição"))
@@ -77,3 +83,5 @@ class Ui_MainWindow(object):
         self.btn_executa_rpa.setText(_translate("MainWindow", "Executar RPA"))
         self.btn_relacionar_rubricas.setText(_translate("MainWindow", "Relacionar Rubricas"))
         self.btn_relaciona_empresas_dominio.setText(_translate("MainWindow", "Relacionar empresas Domínio"))
+        self.btn_excluir.setText(_translate("MainWindow", "Excluir"))
+        self.btn_conexoes.setText(_translate("MainWindow", "Configurar conexões"))
